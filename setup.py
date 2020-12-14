0a6c2cdab08d9694555fc7fcfd37c19b5ad2da84
Yeison <yeison_feza@hotmail.com>
2020-12-13 23:45:48 -0400

    fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES=['audio2numpy==0.1.2','librosa==0.6.3','scipy==1.4.1']
+REQUIRED_PACKAGES=['audio2numpy==0.1.2','librosa==0.6.3', 'numba==0.48','scipy==1.4.1']
 
 setup(
     name='trainer',
