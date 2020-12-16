9233ec318f6700c4c5c77d7b788ec5b218596051
Yeison <yeison_feza@hotmail.com>
2020-12-16 18:29:34 -0400

    dataset length

--- trainer/task.py
+++ trainer/task.py
@@ -20,7 +20,7 @@ def get_args():
     parser.add_argument(
         '--num-epochs',
         type=int,
-        default=1000,
+        default=20,
         help='number of times to go through the data, default=20')
     parser.add_argument(
         '--batch-size',
