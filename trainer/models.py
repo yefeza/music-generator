fa0a9ef7d3467ae80259929011e412267e97b2e4
Yeison <yeison_feza@hotmail.com>
2021-01-06 16:14:05 -0400

    clean code

--- trainer/models.py
+++ trainer/models.py
@@ -377,6 +377,19 @@ def define_generator(n_blocks, lstm_layer):
         model_list.append(models)
     return model_list
 
+# Define the loss functions for the discriminator,
+# which should be (fake_loss - real_loss).
+# We will add the gradient penalty later to this loss function.
+def discriminator_loss(real_img, fake_img):
+    real_loss = tf.reduce_mean(real_img)
+    fake_loss = tf.reduce_mean(fake_img)
+    return fake_loss - real_loss
+
+
+# Define the loss functions for the generator.
+def generator_loss(fake_img):
+    return -tf.reduce_mean(fake_img)
+
 # define composite models for training generators via discriminators
 
 def define_composite(discriminators, generators, latent_dim):
