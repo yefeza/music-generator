10263eb5e4e5ff5f1844ea214e04e708b43ba1a3
Yeison <yeison_feza@hotmail.com>
2021-01-10 15:50:52 -0400

    download data by dimension

--- trainer/task.py
+++ trainer/task.py
@@ -21,7 +21,7 @@ def get_args():
     parser.add_argument(
         '--num-epochs',
         type=int,
-        default=10,
+        default=20,
         help='number of times to go through the data, default=20')
     parser.add_argument(
         '--batch-size',
