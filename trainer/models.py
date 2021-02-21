709aed13c5246f961cb22109e1bbf773529bca28
Yeison <yeison_feza@hotmail.com>
2021-02-20 22:01:45 -0400

    lr 0.001,b1=0.9, b2=0.999

--- trainer/models.py
+++ trainer/models.py
@@ -528,8 +528,8 @@ def define_composite(discriminators, generators, latent_dim):
             discriminator_extra_steps=1,
         )
         wgan1.compile(
-            d_optimizer=Adam(lr=0.001, beta_1=0, beta_2=0.99, epsilon=10e-8),
-            g_optimizer=Adam(lr=0.001, beta_1=0, beta_2=0.99, epsilon=10e-8),
+            d_optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=10e-8),
+            g_optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=10e-8),
             g_loss_fn=generator_loss,
             d_loss_fn=discriminator_loss
         )
@@ -543,8 +543,8 @@ def define_composite(discriminators, generators, latent_dim):
             discriminator_extra_steps=1,
         )
         wgan2.compile(
-            d_optimizer=Adam(lr=0.0005, beta_1=0, beta_2=0.99, epsilon=10e-8),
-            g_optimizer=Adam(lr=0.0005, beta_1=0, beta_2=0.99, epsilon=10e-8),
+            d_optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=10e-8),
+            g_optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=10e-8),
             g_loss_fn=generator_loss,
             d_loss_fn=discriminator_loss
         )
