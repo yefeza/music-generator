2c41f0a07356e0ff8af62e89c0ca7ecba3ff5148
Yeison <yeison_feza@hotmail.com>
2021-02-25 12:02:38 -0400

    continue from epoch 500

--- trainer/models.py
+++ trainer/models.py
@@ -431,7 +431,7 @@ def generator_loss_extra(fake_logits, real_logits):
     delta=tf.math.abs(lamb)
     return delta
     
-def get_saved_model(dimension=(4,750,2), bucket_name="music-gen", epoch_checkpoint=260):
+def get_saved_model(dimension=(4,750,2), bucket_name="music-gen", epoch_checkpoint=500):
     storage_client = storage.Client(project='ia-devs')
     bucket = storage_client.bucket(bucket_name)
     #crear carpeta local si no existe
@@ -461,7 +461,7 @@ def define_composite(discriminators, generators, latent_dim):
     for i in range(len(discriminators)):
         g_models, d_models = generators[i], discriminators[i]
         #precargar pesos previos de un checkpoint
-        if False and i==0:
+        if i==0:
             prev_g_model, prev_d_model=get_saved_model()
             d_models[0].set_weights(prev_d_model.get_weights())
             g_models[0].set_weights(prev_g_model.get_weights())
