3ac2d9ebf930a8c7d43e5304c778f8b0cd77654a
Yeison <yeison_feza@hotmail.com>
2021-03-01 14:10:46 -0400

    resume from epoch 200 to 600

--- trainer/task.py
+++ trainer/task.py
@@ -169,7 +169,7 @@ if __name__ == '__main__':
             plot_losses(history, (gen_shape[-3], gen_shape[-2]))
 
     batch_sizes=[16,8,4,2,2,2,2]
-    epochs_norm=[200,300,200,280,300,350,420]
+    epochs_norm=[400,300,200,280,300,350,420]
     #epochs_norm=[50,60,70,80,90,100,110]
     epochs_fade=[10,15,32,25,30,35,40]
     # load image data
