96c337d9f6dc20b245d1c69527e1d68e3bca9060
Yeison <yeison_feza@hotmail.com>
2021-03-04 00:06:04 -0400

    aumentar capacidad de generador

--- trainer/models.py
+++ trainer/models.py
@@ -342,15 +342,16 @@ def define_generator(n_blocks, latent_dim):
     ly0 = Input(shape=latent_dim)
     featured = Conv2D(1024, (1,5), strides=(1,5), padding='valid', kernel_initializer='he_normal')(ly0)
     # bloque 1 deconvolusion
-    g_1 = Conv2DTranspose(128, (1, 5), strides=(1, 5), padding='valid', kernel_initializer='he_normal')(featured)
-    g_1 = Conv2DTranspose(256, (1, 3), strides=(1, 3), padding='valid', kernel_initializer='he_normal')(g_1)
-    g_1 = Conv2DTranspose(512, (1, 5), strides=(1, 5), padding='valid', kernel_initializer='he_normal')(g_1)
+    g_1 = Conv2DTranspose(256, (1, 5), strides=(1, 5), padding='valid', kernel_initializer='he_normal')(featured)
+    g_1 = Conv2DTranspose(512, (1, 3), strides=(1, 3), padding='valid', kernel_initializer='he_normal')(g_1)
+    g_1 = Conv2DTranspose(1024, (1, 5), strides=(1, 5), padding='valid', kernel_initializer='he_normal')(g_1)
     #unir 4 segundos
     #upsample para trabajar texturas en conjunto
     sumarized_blocks = UpSampling2D()(g_1)
     sumarized_blocks = UpSampling2D()(sumarized_blocks)
+    sumarized_blocks = Conv2D(512, (1,2), strides=(1,2), padding='valid', kernel_initializer='he_normal')(sumarized_blocks)
     sumarized_blocks = Conv2D(256, (1,2), strides=(1,2), padding='valid', kernel_initializer='he_normal')(sumarized_blocks)
-    sumarized_blocks = Conv2D(2, (1,2), strides=(1,2), padding='valid', kernel_initializer='he_normal')(sumarized_blocks)
+    sumarized_blocks = Dense(2)(sumarized_blocks)
     wls = LayerNormalization(axis=[1, 2, 3])(sumarized_blocks)
     model = Model(ly0, wls)
     # store model
