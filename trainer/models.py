6fc45c209067c27c6c7e4fdddd9f4d726623b302
Yeison <yeison_feza@hotmail.com>
2021-03-04 21:15:27 -0400

    volver a conv normal

--- trainer/models.py
+++ trainer/models.py
@@ -355,8 +355,8 @@ def define_generator(n_blocks, latent_dim):
     sumarized_blocks = UpSampling2D()(sumarized_blocks)
     sumarized_blocks = Conv2D(64, (4,4), strides=(4,4), padding='valid', kernel_initializer='he_normal')(sumarized_blocks)
     sumarized_blocks = Conv2D(64, (1,4), strides=(1,4), padding='valid', kernel_initializer='he_normal')(sumarized_blocks)
-    sumarized_blocks = Conv2D(32, (4,150), padding='same', kernel_initializer='he_normal')(sumarized_blocks)
-    sumarized_blocks = Conv2D(2, (4,50), padding='same', kernel_initializer='he_normal')(sumarized_blocks)
+    sumarized_blocks = Conv2D(64, (4,150), padding='same', kernel_initializer='he_normal')(sumarized_blocks)
+    sumarized_blocks = Conv2D(2, (4,150), padding='same', kernel_initializer='he_normal')(sumarized_blocks)
     wls = LayerNormalization(axis=[1, 2, 3])(sumarized_blocks)
     model = Model(ly0, wls)
     # store model
