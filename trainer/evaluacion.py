385a80cf702a1db64a3c2bb56bb23eb32a614d0b
Yeison <yeison_feza@hotmail.com>
2021-06-13 20:10:50 -0400

    ajustes

--- trainer/evaluacion.py
+++ trainer/evaluacion.py
@@ -116,7 +116,7 @@ def load_evaluator(dimension, bucket_name, download, train_dataset, epochs):
         if dimension[0]==4:
             model=evaluadores[0]
             batch_size=64
-            epochs=15
+            epochs=50
         if dimension[0]==8:
             model=evaluadores[1]
             batch_size=32
